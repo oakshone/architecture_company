099ae2742f6d2102061384131ce82cbee6e5efbe
Jackson Nieporte <jacksonlibre@gmail.com>
2025-09-28 17:32:36 -0600

    nav issues

---
+++ data/reviews.js
@@ -0,0 +1,27 @@
+export const reviews = [
+  {
+    "customer_name": "Emily Johnson",
+    "star_rating": "☆☆☆☆☆",
+    "comment": "Absolutely loved it! Great service and quality. Will definitely come back!",
+  },
+  {
+    "customer_name": "Michael Chen",
+    "star_rating": "☆☆☆☆",
+    "comment": "Very satisfied overall. A few small issues, but nothing major.",
+  },
+  {
+    "customer_name": "Sofia Martinez",
+    "star_rating": "☆☆☆",
+    "comment": "It was okay. Met expectations but didnt exceed them.",
+  },
+  {
+    "customer_name": "James O'Neil",
+    "star_rating": "☆",
+    "comment": "Really disappointed. The product didnt match the description.",
+  },
+  {
+    "customer_name": "Ava Thompson",
+    "star_rating": "☆☆☆☆☆",
+    "comment": "Amazing experience! Fast shipping and fantastic customer service.",
+  }
+]
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,43 @@
+import { reviews } from "../data/reviews.js";
+console.log(reviews)
+
+const myTarget = document.querySelector("#cards");
+for (let x = 0; x < reviews.length; x++) {
+
+    // section
+    const mySection = document.createElement('section')
+    const myName = document.createElement('h3')
+    myName.textContent = reviews[x].customer_name
+
+    const myStar = document.createElement('p')
+    myStar.textContent = reviews[x].star_rating
+
+    const myComment = document.createElement('p')
+    myComment.textContent = reviews[x].comment
+
+      // name of customer
+    mySection.appendChild(myName);
+    
+    mySection.appendChild(myStar)
+  
+    
+    // comment of each customer
+    mySection.appendChild(myComment);
+    // add a new card to a page
+    myTarget.appendChild(mySection);
+
+}
+
+const navElement = document.querySelector('#theNav');
+const btnElement = document.querySelector('#theButton');
+
+btnElement.addEventListener('click', () => {
+    btnElement.classList.toggle('open');
+    navElement.classList.toggle('open');
+})
+
+
+
 // current year in footer
 const theTime = new Date();
 document.querySelector('#year').textContent = theTime.getFullYear();
